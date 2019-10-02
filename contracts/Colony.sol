ab01f3fb3eb2795ed4d914f2d2f381f0bb8c4417
Daniel Kronovet <kronovet@gmail.com>
2019-10-02 09:52:56 +0300

    Update in response to reviewer feedback III

--- contracts/Colony.sol
+++ contracts/Colony.sol
@@ -20,7 +20,6 @@ pragma experimental ABIEncoderV2;
 
 import "./ColonyStorage.sol";
 import "./IEtherRouter.sol";
-import "./extensions/ExtensionManager.sol";
 
 
 contract Colony is ColonyStorage, PatriciaTreeProofs {
@@ -240,10 +239,16 @@ contract Colony is ColonyStorage, PatriciaTreeProofs {
     return colonyNetwork.addColonyVersion(_version, _resolver);
   }
 
-  function addExtension(address _manager, bytes32 _extensionId, address _resolver, uint8[] memory _roles)
+  function setExtensionManager(address _extensionManagerAddress)
   public stoppable auth
   {
-    ExtensionManager(_manager).addExtension(_extensionId, _resolver, _roles);
+    IColonyNetwork(colonyNetworkAddress).setExtensionManager(_extensionManagerAddress);
+  }
+
+  function addExtension(bytes32 _extensionId, address _resolver, uint8[] memory _roles)
+  public stoppable auth
+  {
+    IColonyNetwork(colonyNetworkAddress).addExtension(_extensionId, _resolver, _roles);
   }
 
   function addDomain(uint256 _permissionDomainId, uint256 _childSkillIndex, uint256 _parentDomainId) public
@@ -335,7 +340,8 @@ contract Colony is ColonyStorage, PatriciaTreeProofs {
   bytes4 constant SIG_C3 = bytes4(keccak256("setArbitrationRole(uint256,uint256,address,uint256,bool)"));
 
   // Add extension manager
-  bytes4 constant SIG_D0 = bytes4(keccak256("addExtension(address,bytes32,address,uint8[])"));
+  bytes4 constant SIG_D0 = bytes4(keccak256("setExtensionManager(address"));
+  bytes4 constant SIG_D1 = bytes4(keccak256("addExtension(bytes32,address,uint8[])"));
 
   // v3 to v4
   function finishUpgrade() public always {
@@ -365,6 +371,7 @@ contract Colony is ColonyStorage, PatriciaTreeProofs {
 
     // Add extension manager
     colonyAuthority.setRoleCapability(uint8(ColonyRole.Root), address(this), SIG_D0, true);
+    colonyAuthority.setRoleCapability(uint8(ColonyRole.Root), address(this), SIG_D1, true);
   }
 
   function checkNotAdditionalProtectedVariable(uint256 _slot) public view recovery {
